b81b4610e431b1b5477082f78cd64317e318375d
Deepan-106 <deepanbehera40332@gmail.com>
2026-02-05 00:28:22 +0530

    Addedd one new line

--- src/main/java/com/spring/App.java
+++ src/main/java/com/spring/App.java
@@ -11,6 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("Added a new line and pushed it to the git...");
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         Student student1 = (Student) context.getBean("student1", Student.class);
         System.out.println(student1);
