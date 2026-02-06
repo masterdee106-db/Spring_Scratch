f878fba965b3f69206847cad1f0e0107040ffbde
Deepan-106 <deepanbehera40332@gmail.com>
2026-02-07 00:12:45 +0530

    implementing bean-scope concept

--- src/main/java/com/spring/streotype/Employee.java
+++ src/main/java/com/spring/streotype/Employee.java
@@ -1,9 +1,17 @@
 package com.spring.streotype;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+//Here also i am mentioning Bean scope concept which mean you can give scope for the beans
+//1. SingleTone: Only ONE instance of the bean is created per Spring IoC container, and the same instance is shared everywhere it’s used.
+//2. ProtoType: In prototype scope, Spring creates a NEW object every time the bean is requested.
+//3. Request: will discuss later in the http methods topics when come
+//4. Session: Will discuss it later...
+
 @Component
+@Scope("prototype")
 public class Employee {
     @Value("Deepan Behera")
     private String employeeName;
--- src/main/java/com/spring/streotype/Test.java
+++ src/main/java/com/spring/streotype/Test.java
@@ -8,5 +8,16 @@ public class Test {
        ApplicationContext conetext =new ClassPathXmlApplicationContext("streotypeconfig.xml");
        Employee employee1 =conetext.getBean("employee", Employee.class);
         System.out.println(employee1);
+        System.out.println(employee1.hashCode());
+// So here you can see that i am calling 2 times the object hascode and this hashcode is same for both the object
+//        Usually when you creating an object it has some unique hashCode but here I am getting same hashcode means here its creating only one object whatever time you want...
+//        Now go to Employee.class and check over there I am changing the scope to Prototype and then come here
+        Employee employee2 = conetext.getBean("employee", Employee.class);
+        System.out.println(employee2.hashCode());
+
+//758119607
+//1982787818
+//        This is 2 different object created for each time creating object by implementing prototype bean-scope
+
     }
 }
